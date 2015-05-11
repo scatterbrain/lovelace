a358812aa317635253204287a8a16e54dfa005ce
Mikko Hämäläinen <mikko.hamalainen@greyarealabs.com>
2015-05-11 17:34:21 +0300

    First part of tutorial

--- index.ios.js
+++ index.ios.js
@@ -7,46 +7,50 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View,
 } = React;
 
+var MOCKED_MOVIES_DATA = [{title : 'Title', year : 2015, posters : {thumbnail : 'http://i.imgur.com/UePbdph.jpg'}}]; 
+
 var lovelace = React.createClass({
   render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+    var movie = MOCKED_MOVIES_DATA[0];
+      return <View style={styles.container}>
+                <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <Text style={styles.year}>{movie.year}</Text>
+                </View>
+            </View>
+
   }
 });
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  rightContainer : {
+    flex : 1
   },
-  instructions: {
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  year: {
+    textAlign: 'center',
   },
 });
 
